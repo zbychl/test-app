a5a7bf03ac47cc71fa7a0d52f8bc0049a5f6f465
Zbigniew Lukwinski <zbigniew.lukwinski@linux.intel.com>
2022-07-18 01:56:42 +0200

    Prepare for service.
    Add infinite loop and flush to see logs in journal.

--- src/main.c
+++ src/main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "main.h"
 
 
 int main(int argc, char* argv[])
 {
-    printf("Hello Word!\n");
+    while(1)
+    {
+        printf("Hello Word!\n");
+        fflush(NULL);
+        usleep(1000000);
+    }
 
     return 0;
 }
